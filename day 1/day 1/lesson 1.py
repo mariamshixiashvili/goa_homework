acbaa12f89242c07224fdfe12942f9a3f5bf5be6
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-02-11 14:41:32 +0400

    second commit

---
+++ day 1/day 1/lesson 1.py
@@ -0,0 +1,23 @@
+name = "mari"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "mari" არის ცვლადისთვის მნიშვნელობა
+surname = "shixiashvili"
+
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+ #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int (integer) მთელი რიცხვი
+height = 155.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False
+
+isUgly = False # ჯავასკრიპტული comelcase
+
+
+
+print (name + " " + surname + " " +str(age)+ " " + str(height))
