b6ce56fa047aa3d4c00569cf5f042b9bc64ed00a
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-03-17 14:22:43 +0400

    second commit

---
+++ homework .py
@@ -0,0 +1,38 @@
+print(6<7 or 13<23)
+print(5>3 or 23>12)
+print(8<15 or 17>16)
+print(4>3 or 7>3)
+print(5<4 or 16<15)
+
+print(34>23 and 10>9)
+print(15<13 and 16<14)
+print(8<3 and 10<5)
+print(23<12 and 56<17)
+print(10>12 and 17>42)
+
+word = "GOA"
+number = 50
+float = 42.5
+print(type(word))
+print(type(number)) 
+print(type(float))
+
+
+str(input("mariami"))
+
+number = int(input(19))
+
+print("hello" + "mariami" + "welcome to group 19")
+
+
+number1 = int(input(10))
+number2 = int(input(5))
+print(number1 / number2)
+
+print(float("10.5"))
+
+
+
+
+
+
