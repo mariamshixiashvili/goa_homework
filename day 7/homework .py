ab19fb5af70141753dccd91546b2cecf0258d050
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-03-24 14:09:08 +0400

    second commit

---
+++ day 7/homework .py
@@ -0,0 +1,16 @@
+print(9>5)
+print(10<15) 
+print(14>10)
+print(12>5)
+print(14>3)
+
+#True and False = False
+#True or False = True
+
+print(True and False) #False
+print(True or False) #True
+
+str = "basketball"
+int = 20
+float = 10.5
+ print(type("basketball"))
