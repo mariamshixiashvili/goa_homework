30ba2178f4032a79c46ba6daec0a88d9e2299f2d
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-05-11 23:26:52 +0400

    commit

---
+++ day 14/homework.py
@@ -0,0 +1,20 @@
+number = input("enter a number")
+if number == 20:
+    print("მთელი რიცხვი")
+else:
+    print("ეს რიცხვი ლუწია")
+
+if number == 21:
+    print("მთელი რიცხვი")
+else:
+    print("ეს რიცხვი კენტია")
+
+if number == 22:
+    print("მთელი რიცხვი")
+else:
+    print("ეს რიცხვი ლუწია")
+
+if number == 23:
+    print("მთელი რიცხვი") 
+else:
+    print("ეს რიცხვი კენტია")     
