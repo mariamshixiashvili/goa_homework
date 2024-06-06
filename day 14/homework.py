1c8a5896aa2d8be59766860c6f69ae19bda2e8ca
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-06-06 16:19:33 +0400

    commit

---
+++ day 14/homework.py
@@ -0,0 +1,20 @@
+number = input("enter a number")
+if number == 20:
+    print("ეს რიცხვი კენტია")
+else:
+    print("ეს რიცხვი ლუწია")
+
+if number == 21:
+    print("ეს რიცხვი ლუწია")
+else:
+    print("ეს რიცხვი კენტია")
+
+if number == 22:
+    print("ეს რიცხვი კენტია")
+else:
+    print("ეს რიცხვი ლუწია")
+
+if number == 23:
+    print("ეს რიცხვი ლუწია") 
+else:
+    print("ეს რიცხვი კენტია")     
---
+++ day 15/classwork.py
@@ -0,0 +1,6 @@
+num = int(input("enter your num:"))
+
+if num % 5 == 0:
+    print("ეს რიცხვი არის ხუთის ჯერადი")
+else:
+    print("არ არის ხუთის ჯერადი")
