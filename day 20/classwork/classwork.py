e6ab8e2668ac702fd2c0ab668fe0b22872b3b83d
mariamshixiashvili <mariamshixiashvili001@gmail.com>
2024-06-23 14:07:46 +0400

    commit

---
+++ day 20/classwork/classwork.py
@@ -0,0 +1,8 @@
+num = 20
+while num > 0:
+    print("goa")
+    num -= 1
+
+while num < 10:
+    print("hello world")
+    num +=1
---
+++ day 20/homework/homework.py
@@ -0,0 +1,8 @@
+for i in range(1,21):
+    print(i)
+
+num = 0
+while num <= 20:
+    print("ეს არის მაგალითი while loop_ის")
+    num += 1
+
